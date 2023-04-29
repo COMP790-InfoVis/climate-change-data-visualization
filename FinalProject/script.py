ec4424aa216d9145ed814d54ef7a304100f42ed0
geoandr-01 <geoandr@live.unc.eud>
2023-04-29 02:56:55 -0400

    updating script.py logic for new dataset

--- FinalProject/script.py
+++ FinalProject/script.py
@@ -1,35 +1,36 @@
 import csv
 import json
 
-file = open('annual-temp-data-2021.csv', newline='')
+file = open('average_yearly_temperatures_celcius_1901-2021_world.csv', newline='')
 json_file = open('world.geo.json', newline='')
 
 data = json.load(json_file)
-
 
 type(file)
 
 csvreader = csv.reader(file)
 
-header = []
-header = next(csvreader)
-header
+csv_country_names = []
+csv_country_names = next(csvreader)
+del csv_country_names[0] #deleting the "year" column from our countries array
 
 rows = []
 for row in csvreader:
         rows.append(row)
-rows
 
-country_names = []
+matched = []
+not_matched = []
+#for i in range(len(csv_country_names)):
+    #for j in data['features']:
+   #     if(j['properties']['name'])==(csv_country_names[i]):
+            #print(csv_country_names[i] + " ")
 
-#print(header[0])
-for i in range(len(rows)):
-    for j in data['features']:
-        if(j['properties']['name'])==(rows[i][0]):
-            print(rows[i][0]+','+rows[i][1])
-           
+for j in data['features']:
+    for i in range(len(csv_country_names)):
+        if(j['properties']['name'])==(csv_country_names[i]):
+             matched.append(j['properties']['name'])
+print(matched)
 
-#print(rows[0])
 
 #for i in data['features']:
     #print(i['properties']['name'])
