7586099945d2d6bb07b09d5a534d0a72d665c63f
geoandr-01 <geoandr@live.unc.eud>
2023-05-01 03:24:22 -0400

    angular + d3 cloropleth map w/ time animation

---
+++ FinalProject/climate-change-app/src/app/app.component.ts
@@ -0,0 +1,90 @@
+import { Component } from '@angular/core';
+import * as d3 from 'd3v4';
+import * as jsonFile from '../assets/final_world.geo.json';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'climate-change-app';
+  globalData = jsonFile
+  titleTag: any;
+  yearRange = ["1901", "1902", "1903", "1904", "1905", "1906", "1907", "1908", "1909", "1910", "1911", "1912", "1913", "1914", "1915", "1916", "1917", "1918", "1919", "1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927", "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937", "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
+  
+  ngOnInit() {
+    this.createMap(1000, 600, this.globalData)
+
+    let time = 1;
+    let interval = setInterval(() => {
+       if (time <= 121) {
+          this.transitionMap(time)
+          time++;
+       } else {
+          clearInterval(interval);
+       }
+    }, 1000);
+  } 
+
+
+  createMap(width: number, height: number, dataset: Object) {
+    this.titleTag = this.yearRange[0];
+    const margin = {top: 10, right: 30, bottom: 10, left: 30};
+    width = width - margin.left - margin.right;
+    height = height - margin.top - margin.bottom;
+    const projection = d3.geoMercator()
+                        .scale(130)
+                        .center([0,20])
+                        .translate([width / 2, height / 2]);
+    const path = d3.geoPath().projection(projection);
+    const svg = d3.select('.world-map')
+                 .append('svg')
+                 .attr('viewBox', '0 0 1000 600')
+
+  const colorDomain = [-25, 0, 10, 20, 30, 40]
+  const colorLegend = d3.scaleThreshold().range(['#032f60', '#176dae', '#e3a532', '#dd6e6e', '#cc0000']).domain(colorDomain);
+
+  svg.append("g")
+  .selectAll("path")
+  .data(this.globalData.features)
+  .enter()
+  .append("path")
+  .attr('d', path)
+  .style('fill', function(d: any) {
+    const value = d['properties']['temp1901'];
+    if (value) {
+      return colorLegend(d['properties']['temp1901']);
+    } else {
+      return '#ccc';
+  }})
+  .style('stroke', '#fff')
+  .style('stroke-width', '0.5')
+}
+
+transitionMap(i: number) {
+  this.titleTag = this.yearRange[i];
+
+  const svg = d3.select('.world-map');
+  
+  const colorDomain = [-25, 0, 10, 20, 30, 40]
+  const colorLegend = d3.scaleThreshold().range(['#032f60', '#176dae', '#e3a532', '#dd6e6e', '#cc0000']).domain(colorDomain);
+
+svg.selectAll('path')
+   .data(this.globalData.features)
+   .transition()
+   .delay(100)
+   .duration(1000)
+   .style('fill', function(d: any) {
+      var year = i + 1900 
+      var yearName = "temp" + year
+      const value = d['properties'][yearName];
+      console.log(yearName)
+      if (value) {
+        return colorLegend(d['properties'][yearName]);
+      } else {
+        return '#ccc';
+      }
+   })
+}
+}
--- FinalProject/script.py
+++ FinalProject/script.py
@@ -11,35 +11,60 @@ data = json.loads(response.read())
 
 type(file)
 
+#csv file object
 csvreader = csv.reader(file)
 
+#making array of country names
 csv_country_names = []
 csv_country_names = csv_data['country'].tolist()
-#print(csv_country_names)
-#print(csv_country_names) #deleting the "year" column from our countries array
 
+#making array of row objects
 rows = []
 for row in csvreader:
         rows.append(row)
 
+
 matched = []
 not_matched = []
-#for i in range(len(csv_country_names)):
-    #for j in data['features']:
-   #     if(j['properties']['name'])==(csv_country_names[i]):
-            #print(csv_country_names[i] + " ")
 
+#matching csv names to json names
 for j in data['features']:
-    for i in range(len(csv_country_names)):
-        if(j['properties']['name'])==(csv_country_names[i]):
+    for i in range(len(rows)):
+        if(j['properties']['name'])==(rows[i][0]):
              matched.append(j['properties']['name'])
-    if j['properties']['name'] not in matched: 
-        not_matched.append(j['properties']['name'])
-print((not_matched))
+             startingYear = 1901
+             #print(rows[i][0]) 
 
+#populate array containing countries in csv that are not matched to world.geo.json
+for i in range(len(csv_country_names)):
+        if csv_country_names[i] not in matched: 
+            not_matched.append(csv_country_names[i])
+            
+#remove countries not in world.geo.json from rows array containing all csv countries            
+for j in not_matched:            
+    for i in rows:
+        if(j)==(i[0]):
+            rows.remove(i)
 
-#for i in data['features']:
-    #print(i['properties']['name'])
+rows.pop(0) #remove header row
 
+#setting temp190xx to be a property in the world.geo.json features
+for j in data['features']:
+    jsonCountry = j['properties']['name']
+    for i in rows:
+        if(i[0] == jsonCountry):
+            startingYear = 1901
+            for k in range(122):
+                if k != 0: 
+                    tempYear = "temp" + str(startingYear)
+                    j['properties'][tempYear] = i[k]
+                    startingYear = startingYear + 1
+
+json_file = json.dumps(data)
+print(json_file)
 
 file.close()
+
+with open('final_world.geo.json', 'w') as _f:
+    json.dump(json_file, _f)
+
